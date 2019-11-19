88d1a564060e6093ea021bf8d837a5f9e7c35b7c
Kevin Sage <ksagedeveloper@gmail.com>
2019-11-19 17:48:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -127,6 +127,7 @@ end
 
 def num_points_scored(player)
   game_hash.each do |place, team|
+    binding.pry
     team.each do |attribute, data|
       if attribute == :players
         data.each do |baller|
